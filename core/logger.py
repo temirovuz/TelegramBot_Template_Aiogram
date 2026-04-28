7c718a116336367a23900ebcece2690f49e51725
temirovuz <temirov-muhammad@mail.ru>
2026-04-28 12:10:36 +0500

    logger yangilandi xatoliklar tuzaildi

--- core/logger.py
+++ core/logger.py
@@ -5,30 +5,16 @@ import sys
 def setup_logger(level: str = "INFO") -> logging.Logger:
     logging.basicConfig(
         level=getattr(logging, level.upper()),
-        format="%(asctime)s | %(levelname)-8s | %(message)s",
+        format="%(asctime)s | %(name)s | %(levelname)-8s | %(message)s",
         datefmt="%H:%M:%S",
         stream=sys.stdout,
     )
 
-    # Aiogram shovqinini bostirish
-    logging.getLogger("aiogram").setLevel(logging.WARNING)
-    logging.getLogger("aiohttp").setLevel(logging.WARNING)
+    if level.upper() == "DEBUG":
+        logging.getLogger("aiogram").setLevel(logging.DEBUG)
+        logging.getLogger("aiohttp").setLevel(logging.DEBUG)
+    else:
+        logging.getLogger("aiogram").setLevel(logging.INFO)
+        logging.getLogger("aiohttp").setLevel(logging.WARNING)
 
     return logging.getLogger("bot")
-
-
-# # bot.py
-# from config import config
-# from logger import setup_logger
-#
-# logger = setup_logger(level=config.LOG_LEVEL)
-# logger.info("Bot ishga tushdi ✅")
-
-#
-# # Boshqa fayllarda shunchaki:
-# import logging
-# log = logging.getLogger("bot")
-# log.info("Xabar keldi")
-# log.warning("Ogohlantirish")
-# log.error("Xatolik!")
-# log.exception("Traceback bilan xatolik")
